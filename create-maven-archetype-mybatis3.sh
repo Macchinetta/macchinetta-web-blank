b996189db385d7b76f227a20ef91437a9c9975ca
Macchinetta Framework <macchinetta.fw@gmail.com>
2018-02-13 14:29:42 +0900

    Temporary commit version 1.5.0.RELEASE (deprecated)
    This version use Spring Framework and Spring Security that contain known
    valunerability (CVE-2018-1199).
    We are planning to release fixed version in the near future.

--- create-maven-archetype-mybatis3.sh
+++ create-maven-archetype-mybatis3.sh
@@ -1,26 +1,6 @@
 #!/bin/sh
-MODE="MyBatis3"
-KEYWORD="REMOVE THIS LINE IF YOU USE MyBatis3"
-TARGET="src pom.xml"
-DIRNAME=`echo $MODE | tr "[:upper:]" "[:lower:]"`
-
-rm -rf ./tmp
-mkdir -p tmp/src/main/resources
-cp -r infra/$DIRNAME/* tmp/src/main/resources/
-cp -r infra/common/* tmp/src/main/resources/
-rm -rf `/bin/find tmp -name '.svn' -type d `
-
-cp -rn src pom.xml create-maven-archetype.sh tmp/
-pushd tmp
-
-sed -i -e "/$KEYWORD/d" `grep -rIl "$MODE" $TARGET | grep -v '.svn'`
-
+sh change-infra.sh MyBatis3
 sed -i -e "s/macchinetta-web-blank-noorm/macchinetta-web-blank/g" pom.xml
 sed -i -e "s/macchinetta-web-blank-noorm/macchinetta-web-blank/g" create-maven-archetype.sh
 sed -i -e "s|Web Blank Project (No O/R Mapper)|Web Blank Project|g" pom.xml
-mv ./create-maven-archetype.sh ../create-maven-archetype-tmp.sh
-popd
-
-sh create-maven-archetype-tmp.sh "$1"
-
-rm create-maven-archetype-tmp.sh
+sh create-maven-archetype.sh "$1" "$2"
